4345e6f8ecdfe8df1ef154fbc6bc06f3c904fef0
shindekunal42 <149197713+shindekunal42@users.noreply.github.com>
2025-02-01 00:29:15 +0530

    Add files via upload

---
+++ 142. Linked List Cycle II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        bool iscycle=false;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+            iscycle=true;
+            break;
+            }
+        }
+     if(!iscycle){
+        return NULL;
+     }
+     slow=head;
+     while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+     }
+    return slow;
+        }
+    
+};
