373db980ed3b959e9ae3170563ef465e4454c25b
shindekunal42 <149197713+shindekunal42@users.noreply.github.com>
2025-05-05 00:29:30 +0530

    Add files via upload

---
+++ leetcode91.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n == 0 || s[0] == '0') return 0;
+
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            int oneDigit = s[i - 1] - '0';
+            int twoDigits = stoi(s.substr(i - 2, 2));
+
+            if (oneDigit >= 1)
+                dp[i] += dp[i - 1];
+
+            if (twoDigits >= 10 && twoDigits <= 26)
+                dp[i] += dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
